c1f9a5b2771e5feb7adf37929fe01cfcf21daeb6
Olivier Savary Belanger <olivier@galois.com>
2020-03-24 16:57:04 -0400

    Commenting out cons case of compile-rule

--- seec/private/match.rkt
+++ seec/private/match.rkt
@@ -68,7 +68,7 @@
                      (match-rule-tmps compiled)
                      #`(and (pred #,tmp) #,(match-rule-condition compiled))
                      (match-rule-body-transformer compiled)))]
-      [(cons pata patb)
+      #;[(cons pata patb)
        (with-syntax ([elementa #'elementa]
                      [elementb #'elementb])
          (let* ([compileda (compile-rule #'elementa #'pata)]
