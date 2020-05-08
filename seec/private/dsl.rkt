2105ccd2b6cdc186a1c21635b8a432361da606aa
Olivier Savary Belanger <olivier@galois.com>
2020-05-08 11:49:03 -0400

    Renaming, will add some tests up to make-symbolic-var

--- seec/private/dsl.rkt
+++ seec/private/dsl.rkt
@@ -1,7 +1,7 @@
 #lang rosette/safe
 (provide define-lang
          (struct-out lang)
-         (struct-out rel-lang))
+         (struct-out comp))
 
 
 #|
@@ -33,15 +33,15 @@ TODO: fix find-exploit and unsafe
 ; A language consists of:
 ; - scope:lit
 ; - expression: predsyn
-; - valid-expression: expression -> bool
+; - eallowed (valid-expression): expression -> bool
 ; - context: predsyn
-; - valid-context: context -> bool
+; - callowed (valid-context): context -> bool
 ; - behaviors: lit
 ; - program: lit
 ; - bounds: hash map from lit -> integer
 ; - apply (or link): function from context -> expression -> program
 ; - evaluate: function from program -> behavior
-(struct lang (scope expression valid-expression context valid-context behaviors program bounds apply evaluate))
+(struct lang (scope expression eallowed context callowed behaviors program bounds apply evaluate))
 
 (define-syntax define-lang
   (syntax-rules ()
@@ -57,38 +57,41 @@ TODO: fix find-exploit and unsafe
 
 
 
-; A relation between languages consists of:
-; source-lang (abr:s): A lang structure standing in as source 
-; target-lang (abr:t): "                            as target
-; behavior-relation: equivalence class for source-lang and target-lang's behaviors
-; context-relation : "'s context
+; A compiler between languages consists of:
+; source: A lang structure standing in as source 
+; target: "                            as target
+; brel (behavior-relation): equivalence class for source and target's behaviors
+; crel (context-relation) : "'s context
 ; compile: function from source-expression to target-expression
-(struct rel-lang (source-lang target-lang behavior-relation context-relation compile))
+(struct comp (source target brel crel compile))
 
 
 
 
 (define-syntax-rule (make-symbolic-var lang name)
-     #`(#,lang-scope name #,(hash-ref lang-bounds #'name)))
+  (let* ([s (generate-temporary)])
+    #`(let ([s #,lang-scope name #,(hash-ref lang-bounds #'name)])
+      (assert (#,lang-name-pred s))
+      s)))
 
 
 
 
-; find-exploit: {r:rel-lang} r.source-lang.expression -> (rel-target-context * SAT) + ()
+; find-exploit: {r:comp} r.source.expression -> (rel-target-context * SAT) + ()
 ; Solve the following synthesis problem:
 ; (\lambda v).
 ; Exists c2:r.t.context,
 ;   Forall c1:r.s.context,
 ;     r.context-relation(c1, c2) ->
 ;       not (r.behavior-relation(r.s.apply(c1, v), r.t.apply(c2, r.compile(v))))
-(define-syntax-rule (find-exploit rel v)
-                #`(define c1 (make-symbolic-var #,rel-source-lang #,rel-source-lang-context)
-                    (define c2 (make-symbolic-var #,rel-source-lang #,rel-source-lang-context)  
-                      (define b1 (#,rel-source-lang-evaluate c1 v)
-                        (define b2 (#,rel-target-lang-evaluate c2 (rel-compile v))
-                          (define crel (#,rel-context-relation c1 c2)
-                            (define equality (assert (#,rel-behavior-relation b1 b2))
-                              (define sol (synthesize #:forall c1 #:assume crel #:guarantee (assert (!(apply && equality))))
+(define-syntax-rule (find-exploit comp v)
+                #`(define c1 (make-symbolic-var #,comp-source #,comp-source-context)
+                    (define c2 (make-symbolic-var #,comp-source #,comp-source-context)  
+                      (define b1 (#,comp-source-evaluate c1 v)
+                        (define b2 (#,comp-target-evaluate c2 (comp-compile v))
+                          (define ccomp (#,comp-crel c1 c2)
+                            (define equality (assert (#,comp-brel b1 b2))
+                              (define sol (synthesize #:forall c1 #:assume ccomp #:guarantee (assert (!(apply && equality))))
                                 (if (unsat? sol)
                                     (empty)
                                     (define instance (concretize c2 sol)
@@ -96,13 +99,13 @@ TODO: fix find-exploit and unsafe
 
 
 
-; unsafe: rel-lang -> (rel-source-expression * rel-target-context * SAT) + ()
+; unsafe: comp -> (comp-source-expression * comp-target-context * SAT) + ()
 ; (\lambda r).
 ;   Exists v:r.s.expression
 ;     find-exploit r v
-(define-syntax-rule (find-exploitable-component rel)
-                #`(define v (make-symbolic-var #,rel-source-lang #,rel-source-lang-expression)
-                  (match (find-exploit #,rel v)
+(define-syntax-rule (find-exploitable-component comp)
+                #`(define v (make-symbolic-var #,comp-source #,comp-source-expression)
+                  (match (find-exploit #,comp v)
                     [(list c2 sol)
                      (define instance (concretize v sol)
                        (list v c2 sol))]
