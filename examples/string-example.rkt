a05d4d4658083590fdbfaf417bedae440b3c7469
Jennifer Paykin <jpaykin@galois.com>
2020-05-11 22:03:17 -0700

    Fixed bug in make-concrete-term! for strings and characters

--- examples/string-example.rkt
+++ examples/string-example.rkt
@@ -13,43 +13,16 @@
 (define five (constants (NAT 5)))
 
 
-; Previously, I was able to define
 (define hi-desired (constants (STR "hi")))
 (define c-desired (constants (CHAR #\x)))
-; Now, if I try to use these structures, I get an error
-;(displayln hi-desired)
-;(displayln c-desired)
-#;(match hi-desired
-  [(constants (STR s:string)) (print-string s)]
+(displayln hi-desired)
+(displayln c-desired)
+(match hi-desired
+  [(constants (STR s:string)) (displayln s)]
   )
-#;(match c-desired
-  [(constants (CHAR c:char)) (print-char c)]
+(match c-desired
+  [(constants (CHAR c:char)) (displayln c)]
   )
-
-; Instead, I can construct them by wrapping the `string` and `char` functions,
-; which does some magic in `string.rkt`.
-(define hi-current (constants (STR ,(string "hi"))))
-(define c-current (constants (CHAR ,(char #\x))))
-(displayln hi-current)
-(displayln c-current)
-
-; However, pattern matching doesn't work
-#;(match hi-current
-  [(constants (STR s:string)) (print-string s)]
-  )
-#;(match c-current
-  [(constants (CHAR c:char)) (print-char c)]
-  )
-
-
-
-
-
-
-
-
-
-
 
 
 
