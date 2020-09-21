4fe94165f06fc3295d12589c7a7061b84381e924
Jennifer Paykin <jpaykin@galois.com>
2020-09-21 14:59:49 -0700

    Replaced printf/framework.rkt with updated version

--- examples/printf/framework.rkt
+++ examples/printf/framework.rkt
@@ -1,219 +1,239 @@
 #lang seec
 (require (file "syntax.rkt"))
+(require racket/contract)
 (require seec/private/framework)
+
+(require rosette/lib/value-browser) ; debugging
 
 (require (only-in racket/base
                   raise-argument-error
-                  raise-arguments-error))
+                  raise-arguments-error
+                  parameterize))
 
-(current-bitwidth 5)
 
-(define (spec-interpret p)
-  (match p
-    [(cons (printf-lang (args:arglist conf:config)) f)
-     (interp-fmt-safe f args conf)]
-    [_ (raise-argument-error 'spec-interpret
-                              "(cons (printf-lang (arglist config)) (printf-lang fmt))"
-                              p)]
-    ))
+(set-bitwidth 16 8)
 
-(define (impl-interpret p)
-  (match p
-    [(cons (printf-lang (args:arglist conf:config)) f)
-     (interp-fmt-unsafe f args conf)]
-    [_ (raise-argument-error 'impl-interpret
-                              "(cons (printf-lang (arglist config)) (printf-lang fmt))"
-                              p)]
-    ))
 
-; There is probably a better way of doing this
-; I just want to limit the size of config and of the vlist separately
-(define (max-context-size config-size args-size)
-  (lambda (ctx)
-    (match ctx
-       [(printf-lang (args:arglist conf:config))
-        (let ([c* (printf-lang config config-size)]
-              [a* (printf-lang arglist args-size)])
-          (and (equal? conf c*) (equal? args a*)))])))
-
-; only link when the arglist is consistant with the format-string
-; I think a cleaner way of doing this would be
-; context-relation: s.exp -> t.exp -> s.ctx -> t.ctx
-(define (link-context-fmt ctx f)
+; a context is a pair of an arglist and a config
+(define (spec-interpret ctx f)
   (match ctx
-    [(printf-lang (args:arglist conf:config))
-     (if (fmt-consistent-with-arglist? f args)
-         (cons ctx f)
-         (assert #f))]))
+    [(printf-lang (args:arglist cfg:config))
+     (interp-fmt-safe f args cfg)]))
 
+; It usually makes sense to define functions in the printf grammar that take
+; three arguments: a format string, an argument list, and a configuration. This
+; function converts such a function into one that accepts a [printf-lang]
+; program, consisting of the same parts in a different order.
+#;(define/contract (printf-uncurry f)
+  (-> (-> fmt? arglist? config? any)
+      (-> (cons/c context? fmt?) any))
+  (λ (p)
+    (match p
+      [(cons (printf-lang (args:arglist cfg:config)) fmt)
+       (f fmt args cfg)])))
 
-(define (valid-context valid-args valid-config)
-  (lambda (ctx)
-    (match ctx
-      [(printf-lang (args:arglist conf:config))
-       (and (valid-args args) (valid-config conf))])))
-
-
-
-(define (link-context-empty-args acc f)
-  (let* ([args (printf-lang nil)]
-         [conf (printf-lang (,acc mnil))]
-         [ctx (printf-lang (,args ,conf))])
-        (cons ctx f)))
-    
-;; Note: moved the nil-arglist version of printf-spec to its own language since
-;; it has no weird machines
-(define-language printf-spec-simpl
-  #:grammar printf-lang
-  #:expression fmt #:size 3
-  #:context integer #:size 1
-  #:link link-context-empty-args
-  #:evaluate spec-interpret
-  )
 (define-language printf-spec
   #:grammar printf-lang
-  #:expression fmt #:size 4
-  #:context context #:size 5 #:where (max-context-size 5 2)
+  #:expression fmt #:size 3
+  #:context context #:size 5
   #:link cons
-  #:evaluate spec-interpret
+  #:evaluate (λ (p) (interp-fmt-safe (program->fmt p) (program->arglist p) (program->config p)))
   )
-
-
 (define-language printf-impl
   #:grammar printf-lang
-  #:expression fmt #:size 5
-  #:context context #:size 5 #:where (max-context-size 5 2)
-  #:link cons ;link-context-fmt
-  #:evaluate impl-interpret)
-
-
-;; ctx1 in printf-spec is an integer consisting just of the accumulator.
-;; ctx2 in printf-impl is a printf-lang context.
-;; they are related when the accumulator values are the same and the argument list and memory in ctx2 is nil.
-;; we do allow ctx2 to have a non-trivial arglist. If not, there would be no weird machines.
-(define (spec-to-impl-context-relation ctx1 ctx2)
-  (match ctx2
-    [(printf-lang (arglist (acc:integer mnil)))
-     (equal? (bonsai->number acc) (bonsai->number ctx1))]
-    [_ #f]))
-
-(define-compiler spec-to-impl
-  #:source printf-spec
-  #:target printf-impl
-  #:behavior-relation equal?
-  #:context-relation equal? #;spec-to-impl-context-relation
-  #:compile (λ (x) x)
-  )
-
-;; make-query-weird-component
-(begin
-  (displayln "Trying to find a format string with weird behavior")
-  (let* ([gen (make-query-weird-component spec-to-impl)]
-         [witness (time (gen))])
-    (display-weird-component witness displayln)))
-
-
-
-;make-query-exploit-gadget
-(define (valid-conf prog)
-  (match prog
-    [(cons ctx f)     
-     (match ctx
-       [(printf-lang (args:arglist conf:config))
-        (match (interp-fmt-unsafe f args conf)
-          [(list str conf+) (conf? conf+)])])]))
-     
-
-(define (fmt-consistent-with-arglist?-uncurry prog)
-    (match prog
-      [(cons ctx f)
-       (match ctx
-         [(printf-lang (args:arglist conf:config))
-          (fmt-consistent-with-arglist? f args)])]))
-
-
-
-(define (is-constant-add f c args conf)
-  (let ([conf+ (behavior->config (interp-fmt-safe f args conf))])
-    (equal? (conf->acc conf+) (+ c (conf->acc conf)))))
-
-; TODO: fix: this function doesn't currently actually use beh and instead re-calls intepr-fmt-safe
-(define (is-constant-add-spec prog beh)
-    (match prog
-    [(cons ctx f)
-     (match ctx
-       [(printf-lang (args:arglist conf:config))
-        (is-constant-add f 1 args conf)])]))
-
-
-(begin
-  (displayln "Trying to make-query-add-constant using the framework")
-  (let* ([gen (make-query-gadget printf-spec fmt-consistent-with-arglist?-uncurry is-constant-add-spec)]
-         [witness (gen)])
-    (display-gadget witness displayln)))
-
-(define (is-constant-add-positive f c args conf)
-  (let* ([conf+ (behavior->config (interp-fmt-safe f args conf))]
-         [acc   (conf->acc conf)]
-         [acc+  (conf->acc conf+)]
-        )
-    (equal? acc+ (+ (max c 0) acc))
-    ))
-
-(define (is-constant-add-positive-spec prog beh)
-  (match (cons beh prog)
-    [(cons (printf-lang (t:trace conf+:config))
-                 ; assume that the arglist has the form (cons x args+),
-                 ; where x is the value being added to the accumulator
-           (cons (printf-lang ((cons x:integer args+:arglist) conf:config))
-                 f))
-     (let ([acc+ (conf->acc conf+)]
-           [acc  (conf->acc conf)]
-           )
-         
-     (equal? acc+ (+ (max (bonsai->number x) 0) acc)))
-     ]
-    ))
-     
-
-
-#;(define (is-constant-add-link x-val ctx f)
-  (define args (match ctx
-                 [(printf-lang (args:arglist config)) args]
-                 ))
-  (define args+ (printf-lang arglist 3))
-  (assert (equal? args (ll-cons (bonsai-integer x-val) args+)))
-  (cons ctx f)
+  #:expression fmt #:size 3
+  #:context context #:size 5
+  #:link cons
+  #:evaluate (λ (p) (interp-fmt-unsafe (program->fmt p) (program->arglist p) (program->config p)))
   )
 
 
+(define (valid-printf-spec-program? f args cfg)
+  (fmt-consistent-with-arglist? f args))
 
-(define (context-is-x config-size arg-size x-val)
-  (λ (ctx)
-    (match ctx
-      [(printf-lang (args:arglist conf:config))
-       (let ([c* (printf-lang config config-size)]
-             [a+ (printf-lang arglist (- arg-size 1))]
-             )
-         (and (equal? conf c*)
-              (equal? args (ll-cons (bonsai-integer x-val) a+))
-              ))])))
-      
-(begin 
-  (define-symbolic x-val integer?)
-  (define-language printf-is-constant-add
-    #:grammar printf-lang
-    #:expression fmt #:size 4
-    #:context context #:size 5 #:where (context-is-x 5 2 x-val)
-    #:link cons
-    #:evaluate spec-interpret
+(define/contract (bv-add-integer b x)
+  (-> bv? integer? bv?)
+  #;(printf "(bv-add-integer ~a ~a)~n" b x)
+  (bvadd b (bonsai-bv-value (integer->bonsai-bv x)))
+  )
+
+(define/contract (add-constant-spec c p res)
+  (-> integer? printf-program? behavior? boolean?)
+  #;(printf "(add-constant-spec ~a ~a ~a)~n" c p res)
+  (let* ([acc (conf->acc (program->config p))]
+         [acc+ (conf->acc (behavior->config res))]
+         )
+    (equal? acc+ (bv-add-integer acc c))
+    ))
+
+
+(define (symbolic? e)
+  (not (equal? (symbolics e) (list ))))
+
+
+
+
+(define (find-increment-gadget)
+  (define g (find-gadget printf-spec
+                         ((curry add-constant-spec) 1)
+                         ))
+  (display-gadget g displayln)
+  )
+(find-increment-gadget)
+
+
+
+(define (find-add-constant-gadget c)
+
+  (define g (find-gadget printf-spec
+                         ((curry add-constant-spec) c)
+                         #:expr-bound 5
+                         #:context-bound 3
+                         ; NOTE: will not find gadget without this context-constraint. WHY????
+                         #:context-constraint (λ (ctx) (match (context->arglist ctx)
+                                                         [(printf-lang (cons s:string arglist))
+                                                          ; need to compare the
+                                                          ; string via equal?
+                                                          ; because pattern
+                                                          ; matching against
+                                                          ; string literals does
+                                                          ; not work currently
+                                                          (equal? s (printf-lang ""))]
+                                                         [_ #f]))
+                         ))
+  (display-gadget g displayln)
+  )
+(find-add-constant-gadget 100)
+
+(define (find-add-argument-gadget)
+
+  (define/contract f-concrete fmt?
+    (printf-lang (cons (% (0 $) (* 1) s) nil)))
+  (define/contract (args-concrete y-val) (-> integer? arglist?)
+    (printf-lang (cons "" (cons ,(integer->bonsai-bv y-val) nil))))
+  (define/contract (context-concrete y-val)
+    (-> integer? context?)
+    (printf-lang (,(args-concrete y-val)
+                  ((bv 0) mnil))))
+
+  #;(parameterize ([debug? #t])
+    (define res (interp-fmt-unsafe f-concrete (args-concrete 128) (printf-lang ((bv 0) mnil))))
+    (define spec (add-constant-spec 128 (make-program f-concrete (args-concrete 128)
+                                                 (printf-lang ((bv 0) mnil)))
+                                    res))
+    (printf "spec: ~a~n" spec)
     )
 
-  (displayln "Trying to find a format string that adds the value of a positive
-number x to the accumulator")
-  
-  (define sol (find-gadget printf-is-constant-add
-                               fmt-consistent-with-arglist?-uncurry
-                               (is-constant-add-positive-spec x-val)))
-  (display-gadget sol displayln)
+  (define-symbolic x-val integer?)
+  (define-symbolic acc-val integer?)
+  (display-gadget
+   (find-gadget printf-impl
+                       ((curry add-constant-spec) x-val)
+                       #:valid (λ (p) (fmt-consistent-with-arglist? (program->fmt p)
+                                                                    (program->context p)))
+                       #:expr-bound 5
+                       #:context-bound 4 ; must be at least 4
+                       #:context-constraint (λ (ctx) 
+                                              (and (match (context->arglist ctx)
+                                                     ; NOTE: don't want to
+                                                     ; quantify over the entire
+                                                     ; context, since we want to
+                                                     ; figure out what x and s should be!
+                                                     [(printf-lang (cons s:string
+                                                                   (cons x:bvint arglist)))
+                                                      (equal? x (integer->bonsai-bv x-val))
+                                                      ]
+                                                     [_ #f])
+                                                   (equal? (integer->bonsai-bv acc-val)
+                                                    (bonsai-bv (conf->acc (context->config ctx))))
+                                                   )
+                                               )
+                       #:forall (list x-val acc-val)
+                       )
+   displayln)
   )
+(find-add-argument-gadget)
+
+
+(define (find-load-gadget)
+
+  (define/contract f-concrete fmt?
+    (printf-lang (cons (% (0 $) (* 1) s) nil)))
+  (define args+ (printf-lang arglist 2))
+  (define/contract (args-concrete l) (-> ident? arglist?)
+    (printf-lang (cons "" (cons (* (LOC ,l)) ,args+))))
+  (define m+ (printf-lang mem 2))
+  (define/contract (mem-concrete l y-val)
+    (-> ident? integer? mem?)
+    (printf-lang (mcons ,l ,(integer->bonsai-bv y-val) ,m+)))
+
+    
+  (define/contract (context-concrete l y-val acc-val)
+    (-> ident? integer? integer? context?)
+    (printf-lang (,(args-concrete l)
+                  (,(integer->bonsai-bv acc-val)
+                   ,(mem-concrete l y-val)
+                   ))))
+
+  #;(parameterize ([debug? #t])
+    (define res (interp-fmt-unsafe f-concrete (args-concrete 128) (printf-lang ((bv 0) mnil))))
+    (define spec (add-constant-spec 128 (make-program f-concrete (args-concrete 128)
+                                                 (printf-lang ((bv 0) mnil)))
+                                    res))
+    (printf "spec: ~a~n" spec)
+    )
+
+  (define-symbolic x-val integer?)
+  (define-symbolic acc-val integer?)
+  (define-symbolic l-val integer?)
+  (define/contract l ident? (bonsai-integer l-val))
+  #;(define l-val (printf-lang ident 1))
+  (display-gadget
+   (find-gadget printf-impl
+                       ((curry add-constant-spec) x-val)
+                       #:valid (λ (p) (fmt-consistent-with-arglist? (program->fmt p)
+                                                                    (program->context p)))
+                       #:expr-bound 5
+;                       #:expr f-concrete
+;                       #:expr-constraint (λ (f) (equal? f f-concrete))
+                       #:context-bound 6
+                       ; NOTE: I found it easier to provide this "concrete"
+                       ;   context with symbolic variables in it, than to
+                       ;   provide a completely symbolic context and constrain
+                       ;   it. Not only did the completely symbolic version take
+                       ;   multiple minutes as opposed to less than 1 minute,
+                       ;   but it provided a bogus answer. Debugging this would
+                       ;   be useful, but in the meantime providing sketches is
+                       ;   a reasonable compromise.
+                       #:context (context-concrete l x-val acc-val)
+                       #:fresh-witness #f
+#|
+                       #:context-constraint (λ (ctx) 
+                                              (and (match (context->arglist ctx)
+                                                     ; NOTE: ideally we could
+                                                     ; just say that l occurs in
+                                                     ; the arglist, not exactly
+                                                     ; the shape of the arglist
+                                                     [(printf-lang (cons s:string
+                                                                   (cons (* (LOC l+:ident))
+                                                                   arglist)))
+                                                      (equal? l+ l)
+                                                      ]
+                                                     [_ #f])
+                                                   (equal? (integer->bonsai-bv acc-val)
+                                                    (bonsai-bv (conf->acc (context->config ctx))))
+                                                   (match (conf->mem (context->config ctx))
+                                                     [(printf-lang (mcons l+:ident v+:bvint mem))
+                                                      (and (equal? l+ l)
+                                                           (equal? v+ (integer->bonsai-bv x-val)))]
+                                                     [_ #f])
+                                                   )
+                                               )
+|#                      
+                       #:forall (list l-val x-val acc-val)
+                       )
+   displayln)
+  )
+#;(find-load-gadget)
+
+
